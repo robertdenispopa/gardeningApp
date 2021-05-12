8b5750d4cc00e4b920f1c5655e917bd45286ed48
Robert-Denis Popa <robertdenispopa@gmail.com>
2021-05-12 16:26:09 +0300

    Creating simple design

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,8 +1,21 @@
 <template>
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <hello/>
-    <info/>
+  <div class="app">
+    <header>
+      <div class="app-bar">
+        <div class="app-header">
+          <h1>🌱 gardeningApp</h1>
+        </div>
+        <div class="app-menu">
+          <p class="item"> Products </p>
+          <p class="item"> Orders </p>
+          <p class="item"> Profile </p>
+        </div>
+        <div class="login">
+          <p class="item">Account</p>
+        </div>
+      </div>
+    </header>
+
   </div>
 </template>
 
