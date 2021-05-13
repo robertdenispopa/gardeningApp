f12437896f67cf0d26b9b8077c49ab4387725711
Robert-Denis Popa <robertdenispopa@gmail.com>
2021-05-14 00:28:19 +0300

    Cart almost done

---
+++ imports/ui/components/CartElement.vue
@@ -0,0 +1,24 @@
+<template>
+        <tr>
+            <td>{{ this.cartElement.name}}</td>
+            <td>{{ this.cartElement.description}}</td>
+            <td>{{ this.cartElement.price}}</td>
+        </tr>   
+</template>
+
+<script>
+    import Vue from "vue";
+    import { CartCollection } from "../../api/collections/CartCollection.js";
+
+    export default {
+    props: ["cartElement"],
+        data() {
+        return {};
+        },
+    computed: {
+        
+    },
+    methods: {
+    }
+};
+</script>
